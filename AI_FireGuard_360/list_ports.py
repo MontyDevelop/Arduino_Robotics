b33f28c55312b27e74868b64be2ba72607a26ec0
MontyDevelop <devmonty3@gmail.com>
2026-09-03 12:16:03 +0530

    Blinking of LED

--- AI_FireGuard_360/list_ports.py
+++
@@ -1,7 +0,0 @@
-import serial.tools.list_ports
-
-ports = list(serial.tools.list_ports.comports())
-if not ports:
-    print("No serial ports found.")
-for p in ports:
-    print(f"{p.device} - {p.description}")
